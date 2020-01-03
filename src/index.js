d7d0e7212a1da2b76a40b8d38f7b486dd284bfca
robbie10001/rn <robbie_c1000@hotmail.com>
2020-01-03 12:27:41 +1100

    added dependencies from app build

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import React from "react"; 
 import ReactDOM from "react-dom";
+import Express from "express";
+import Mongoose from "mongoose"; 
+import Mongo from "mongodb"; 
+import Nodemon from "nodemon"; 
+import ReactrouterDOM from "react-router-dom";
+
+
 
