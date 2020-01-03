f9c8f470161a36021b6274ef7a8a364e0ea74fb7
robbie10001/rn <robbie_c1000@hotmail.com>
2020-01-03 13:04:07 +1100

    Need to talk to Bruce about errors with Express and Nodemon

--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
 import React from "react"; 
 import ReactDOM from "react-dom";
-import Express from "express";
 import Mongoose from "mongoose"; 
 import Mongo from "mongodb"; 
-import Nodemon from "nodemon"; 
+//import Express from "express";
+//import Nodemon from "nodemon"; 
+//we have identified that express and nodemon are preventing our application from rendering. 
 import ReactrouterDOM from "react-router-dom";
 
 
 
+
+
+const App = () => {
+    return (
+        <div className="ui container comments">
+   
+                <div>
+                    <h4>Warning</h4>
+                Are you sure you want to do this?
+                </div>
+                author="Sam" 
+                timeAgo="Today at 4.45pm" 
+                comment="What a world!"
+                avatar
+                //note props are passed like in the above code.
+  
+          
+        </div>
+    );
+};
+
+
+
+
+ReactDOM.render(<App />, document.querySelector("#root"))
